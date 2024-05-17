24595b626e13e5f9463366facf38b4bae5ad783a
shuja <shuja.hafez@gmail.com>
2024-05-17 21:44:00 +0530

    redirected output

--- frontend.sh
+++ frontend.sh
@@ -3,7 +3,7 @@ systemctl enable nginx &>>/tmp/expense.log
 systemctl start nginx &>>/tmp/expense.log
 rm -rf /usr/share/nginx/html/*
 cp expense.conf /etc/nginx/default.d/expense.conf
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip  &>>/tmp/expense.log
 cd /usr/share/nginx/html &>>/tmp/expense.log
 unzip /tmp/frontend.zip &>>/tmp/expense.log
 systemctl restart nginx &>>/tmp/expense.log
