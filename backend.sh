36e1ee31ebd074d635a814b2143b5669d6c4e94c
shuja <shuja.hafez@gmail.com>
2024-05-17 20:42:53 +0530

    cd /app

--- backend.sh
+++ backend.sh
@@ -3,7 +3,7 @@ dnf module enable nodejs:20 -y
 dnf install nodejs -y
 useradd expense
 mkdir /app
-rm -rf /app
+rm -rf /tmp/backend.zip
 cp backend.service /etc/systemd/system/backend.service
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
 cd /app
