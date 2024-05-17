203de4a875191d6ead5f3803383ef7cefdb84b7b
shuja <shuja.hafez@gmail.com>
2024-05-17 20:28:20 +0530

    changed order of cp backend.servive

--- backend.sh
+++ backend.sh
@@ -3,12 +3,11 @@ dnf module enable nodejs:20 -y
 dnf install nodejs -y
 useradd expense
 mkdir /app
+cp backend.service /etc/systemd/system/backend.service
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
-cd /app
 unzip /tmp/backend.zip
 cd /app
 npm install
-cp backend.service /etc/systemd/system/backend.service
 systemctl daemon-reload
 systemctl enable backend
 systemctl start backend
