b2f8349b0fb20138ed17bdd765c9a70f853d8852
shuja <shuja.hafez@gmail.com>
2024-05-18 12:06:37 +0530

    print_task_heading added and source also added

--- frontend.sh
+++ frontend.sh
@@ -1,27 +1,28 @@
-echo "Install Nginx"
+source common.sh
+print_Task_heading "Install Nginx"
 dnf install nginx -y &>>/tmp/expense.log
 echo $?
-echo "Enable Nginx"
+print_Task_heading "Enable Nginx"
 systemctl enable nginx &>>/tmp/expense.log
 echo $?
-echo "Starting Nginx"
+print_Task_heading "Starting Nginx"
 systemctl start nginx &>>/tmp/expense.log
 echo $?
-echo "Removing Default contents"
+print_Task_heading "Removing Default contents"
 rm -rf /usr/share/nginx/html/*
 echo $?
-echo "Copying configuration"
+print_Task_heading "Copying configuration"
 cp expense.conf /etc/nginx/default.d/expense.conf
 echo $?
-echo "Downloading app contents"
+print_Task_heading "Downloading app contents"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip  &>>/tmp/expense.log
 echo $?
-echo "Changing Dir"
+print_Task_heading "Changing Dir"
 cd /usr/share/nginx/html &>>/tmp/expense.log
 echo $?
-echo "unzip frontend contents"
+print_Task_heading "unzip frontend contents"
 unzip /tmp/frontend.zip &>>/tmp/expense.log
 echo $?
-echo "Restarting Nginx"
+print_Task_heading "Restarting Nginx"
 systemctl restart nginx &>>/tmp/expense.log
 echo $?
--- mysql.sh
+++ mysql.sh
@@ -1,12 +1,13 @@
-echo "Installing Mysql server"
+source common.sh
+print_Task_heading "Installing Mysql server"
 dnf install mysql-server -y &>>/tmp/expense.log
 echo $?
-echo "Enabling mysqld"
+print_Task_heading "Enabling mysqld"
 systemctl enable mysqld &>>/tmp/expense.log
 echo $?
-echo "Starting mysqld"
+print_Task_heading "Starting mysqld"
 systemctl start mysqld  &>>/tmp/expense.log
 echo $?
-echo "Setting up Password"
+print_Task_heading "Setting up Password"
 mysql_secure_installation --set-root-pass ExpenseApp@1  &>>/tmp/expense.log
 echo $?
