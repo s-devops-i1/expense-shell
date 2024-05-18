697698f763ecb11876ef301e9f813f0eed640829
shuja <shuja.hafez@gmail.com>
2024-05-18 11:57:51 +0530

    print_task_heading function added

--- backend.sh
+++ backend.sh
@@ -1,41 +1,45 @@
+print_Task_heading(){
+  echo $1
+  "######## $1 #####" &>>/tmp/expense.log
+}
 dnf module disable nodejs -y  &>>/tmp/expense.log
 dnf module enable nodejs:20 -y  &>>/tmp/expense.log
-echo "Install nodejs"
+print_Task_heading "Installing nodejs"
 dnf install nodejs -y &>>/tmp/expense.log
 echo $?
-echo "Add user"
+print_Task_heading "Add user"
 useradd expense
 echo $?
-echo "Create Directory"
+print_Task_heading "Create Directory"
 rm -rf /app
 mkdir /app
 echo $?
-echo "Remove Old contents"
+print_Task_heading "Remove Old contents"
 rm -rf /tmp/backend.zip
 echo $?
-echo "copy backend services to systemd"
+print_Task_heading "copy backend services to systemd"
 cp backend.service /etc/systemd/system/backend.service
 echo $?
-echo "Download backend app"
+print_Task_heading "Download backend app"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/expense.log
 echo $?
-echo "change dir"
+print_Task_heading "change dir"
 cd /app
 echo $?
-echo "Unzip backend contents"
+print_Task_heading "Unzip backend contents"
 unzip /tmp/backend.zip &>>/tmp/expense.log
 echo $?
-echo "Download dependencies"
+print_Task_heading "Download dependencies"
 npm install &>>/tmp/expense.log
 echo $?
-echo "Starting backend app"
+print_Task_heading "Starting backend app"
 systemctl daemon-reload
 systemctl enable backend &>>/tmp/expense.log
 systemctl start backend
 echo $?
-echo "Installing mysql client"
+print_Task_heading "Installing mysql client"
 dnf install mysql -y &>>/tmp/expense.log
 echo $?
-echo "Setting up mysql Initial password"
+print_Task_heading "Setting up mysql Initial password"
 mysql -h 54.152.222.238 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>/tmp/expense.log
 echo $?
