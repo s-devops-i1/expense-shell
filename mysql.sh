5a6fd79c53168a637af1def6334ebc9705014286
shuja <shuja.hafez@gmail.com>
2024-05-18 11:44:41 +0530

    added all heading to frontend

--- mysql.sh
+++ mysql.sh
@@ -1,4 +1,11 @@
-dnf install mysql-server -y
-systemctl enable mysqld
-systemctl start mysqld
-mysql_secure_installation --set-root-pass ExpenseApp@1
+echo "Installing Mysql server"
+dnf install mysql-server -y &>>/tmp/expense.log
+echo $?
+echo "Enabling mysqld"
+systemctl enable mysqld &>>/tmp/expense.log
+echo $?
+echo "Starting mysqld"
+systemctl start mysqld  &>>/tmp/expense.log
+echo $?
+mysql_secure_installation --set-root-pass ExpenseApp@1  &>>/tmp/expense.log
+echo $?
