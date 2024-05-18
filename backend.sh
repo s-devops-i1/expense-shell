cfe3fcdea36e0f041b7ee68f671ccbd0fe483c46
shuja <shuja.hafez@gmail.com>
2024-05-18 11:25:33 +0530

    exit status added

--- backend.sh
+++ backend.sh
@@ -1,28 +1,40 @@
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
+dnf module disable nodejs -y  &>>/tmp/expense.log
+dnf module enable nodejs:20 -y  &>>/tmp/expense.log
 echo "Install nodejs"
-dnf install nodejs -y
+dnf install nodejs -y &>>/tmp/expense.log
+echo $?
 echo "Add user"
 useradd expense
+echo $?
 echo "Create Directory"
 mkdir /app
+echo $?
 echo "Remove Old contents"
 rm -rf /tmp/backend.zip
+echo $?
 echo "copy backend services to systemd"
 cp backend.service /etc/systemd/system/backend.service
+echo $?
 echo "Download backend app"
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/expense.log
+echo $?
 echo "change dir"
 cd /app
+echo $?
 echo "Unzip backend contents"
-unzip /tmp/backend.zip
+unzip /tmp/backend.zip &>>/tmp/expense.log
+echo $?
 echo "Download dependencies"
-npm install
+npm install &>>/tmp/expense.log
+echo $?
 echo "Starting backend app"
 systemctl daemon-reload
-systemctl enable backend
+systemctl enable backend &>>/tmp/expense.log
 systemctl start backend
+echo $?
 echo "Installing mysql client"
-dnf install mysql -y
+dnf install mysql -y &>>/tmp/expense.log
+echo $?
 echo "Setting up mysql Initial password"
-mysql -h 54.152.222.238 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h 54.152.222.238 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>/tmp/expense.log
+echo $?
