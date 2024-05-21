c77949bb3e9316be7f33fc78605086514412c59b
shuja <shuja.hafez@gmail.com>
2024-05-21 22:30:36 +0530

    clearing passwd

--- mysql.sh
+++ mysql.sh
@@ -11,9 +11,12 @@ print_task_heading "Starting mysql"
 systemctl start mysqld &>>${LOG}
 print_status $?
 print_task_heading "Setting up root password"
+
 if [ -z ${setting_root_password}  ]; then
    echo -e "\e[33mPlease provide password\e[0m"
  else
    mysql_secure_installation --set-root-pass ${setting_root_password} &>>${LOG}
  fi
+
 print_status $?
+rm -rf ${setting_root_password}
