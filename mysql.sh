1e7a6028a93cd457245b9fe6b6f7670a2ce80b97
shuja <shuja.hafez@gmail.com>
2024-05-21 22:19:48 +0530

    mysql setup

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,14 @@
-dnf install mysql-server -y &>>/tmp/expense.log
-echo $?
-systemctl enable mysqld &>>/tmp/expense.log
-echo $?
-systemctl start mysqld &>>/tmp/expense.log
-echo $?
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>/tmp/expense.log
+source common.sh
+
+print_task_heading "Install mysql-server"
+dnf install mysql-server -y &>>${LOG}
+print_status $?
+print_task_heading "Enable mysql"
+systemctl enable mysqld &>>${LOG}
+print_status $?
+print_task_heading "Starting mysql"
+systemctl start mysqld &>>${LOG}
+print_status $?
+print_task_heading "Setting up root password"
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>${LOG}
+print_status $?
