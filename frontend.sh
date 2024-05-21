f5ff893b12fcb42c616288955c9e0d6e47ce75d8
shuja <shuja.hafez@gmail.com>
2024-05-21 22:14:12 +0530

    status and heading func added to frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -1,12 +1,25 @@
+print_task_heading "Install Nginx"
 dnf install nginx -y  &>>/tmp/expense.log
+print_status $?
 systemctl enable nginx &>>/tmp/expense.log
+print_task_heading "Starting Nginx"
 systemctl start nginx
+print_status $?
+print_task_heading "Removing old contents"
 rm -rf /usr/share/nginx/html/*
+print_status $?
+print_task_heading "Copying config"
 cp expense.conf /etc/nginx/default.d/expense.conf
+print_status $?
+print_task_heading "Downloading frontend contents"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip &>>/tmp/expense.log
+print_status $?
 cd /usr/share/nginx/html
+print_task_heading "Extracting frontend contents"
 unzip /tmp/frontend.zip &>>/tmp/expense.log
-
+print_status $?
+print_task_heading "Restarting Nginx"
 systemctl restart nginx &>>/tmp/expense.log
+print_status $?
 
 
