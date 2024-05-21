a1a0331f78a45eb358907f2208b07718cf246bd4
shuja <shuja.hafez@gmail.com>
2024-05-21 21:54:30 +0530

    print_task_heading func added

--- backend.sh
+++ backend.sh
@@ -1,16 +1,16 @@
 source common.sh
 
 print_task_heading "Disable nodejs"
-dnf module disable nodejs -y &>>/tmp/expense.log
+dnf module disable nodejs -y &>>$LOG
 print_status $?
 print_task_heading "Enable nodejs"
-dnf module enable nodejs:20 -y &>>/tmp/expense.log
+dnf module enable nodejs:20 -y &>>$LOG
 print_status $?
 print_task_heading "Install nodejs"
-dnf install nodejs -y &>>/tmp/expense.log
+dnf install nodejs -y &>>$LOG
 print_status $?
 print_task_heading "Creating User"
-UID=$(id)
+UID=$(id) &>>$LOG
 if [ "${UID}" != 0 ]; then
    useradd expense
  fi
@@ -23,23 +23,23 @@ print_task_heading "Copying config"
 cp backend.service /etc/systemd/system/backend.service
 print_status $?
 print_task_heading "Download backend contents"
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>/tmp/expense.log
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/expense-backend-v2.zip &>>$LOG
 print_status $?
 cd /app
 print_task_heading "Extracting "
-unzip /tmp/backend.zip &>>/tmp/expense.log
+unzip /tmp/backend.zip &>>$LOG
 print_status $?
 print_task_heading "Install nodejs dependencies"
-npm install
+npm install &>>$LOG
 print_status $?
 print_task_heading "Starting backend service"
 systemctl daemon-reload
-systemctl enable backend &>>/tmp/expense.log
+systemctl enable backend &>>$LOG
 systemctl start backend
 print_status $?
 print_task_heading "Installing mysql"
-dnf install mysql -y &>>/tmp/expense.log
+dnf install mysql -y &>>$LOG
 print_status $?
 print_task_heading "Setting Up Password"
-mysql -h 172.31.33.255 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>/tmp/expense.log
+mysql -h 172.31.33.255 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$LOG
 print_status $?
