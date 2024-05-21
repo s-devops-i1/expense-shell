690260af6aebc75554398af032a0d4bca4688684
shuja <shuja.hafez@gmail.com>
2024-05-21 17:21:46 +0530

    changed the order of copy conf file

--- frontend.sh
+++ frontend.sh
@@ -1,22 +1,29 @@
 source common.sh
+
 print_Task_heading "Install Nginx"
 dnf install nginx -y &>>$LOG
 print_status $?
-print_Task_heading "Copying configuration"
-cp expense.conf /etc/nginx/default.d/expense.conf
-print_status $?
+
 print_Task_heading "Removing Default contents"
 rm -rf /usr/share/nginx/html/*
 print_status $?
+
+print_Task_heading "Copying configuration"
+cp expense.conf /etc/nginx/default.d/expense.conf
+print_status $?
+
 print_Task_heading "Downloading app contents"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip  &>>$LOG
 print_status $?
+
 print_Task_heading "Changing Dir"
 cd /usr/share/nginx/html &>>$LOG
 print_status $?
+
 print_Task_heading "unzip frontend contents"
 unzip /tmp/frontend.zip &>>$LOG
 print_status $?
+
 print_Task_heading "Enable Nginx"
 systemctl enable nginx &>>$LOG
 print_status $?
